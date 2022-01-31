4a485631a91815a22bb21636c235dd7a87747137
Jeff Everhart <Jeffeverhart383@gmail.com>
2022-01-31 16:06:27 -0500

    add defaults to gitignore, update hero text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
       <main className="content">
         <Hero
-          title="Get Started with Headless"
+          title="Jeff's Test Deployment"
           buttonText="Developer Docs"
           buttonURL="https://faustjs.org"
           button2Text="Headless on GitHub"
