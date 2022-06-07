7b745e9c0db3ee1f289668bea0f515a8c3b42a68
Jeff Everhart <Jeffeverhart383@gmail.com>
2022-06-07 15:40:29 -0400

    add sitemap middleware

---
+++ src/pages/_middleware.tsx
@@ -0,0 +1,24 @@
+import { handleSitemapRequests } from '@faustjs/next';
+import { NextRequest, NextResponse } from 'next/server';
+
+export default async function _middleware(req: NextRequest) {
+  const sitemapRequest = await handleSitemapRequests(req, {
+    wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
+    sitemapIndexPath: `/sitemap_index.xml`,
+    sitemapPathsToIgnore: ['/author-sitemap.xml'],
+    async robotsTxt(sitemapUrl) {
+      return `
+        User-agent: *
+        Allow: /
+
+        Sitemap: ${sitemapUrl}
+      `;
+    },
+  });
+
+  if (sitemapRequest) {
+    return sitemapRequest;
+  }
+
+  return NextResponse.next();
+}
