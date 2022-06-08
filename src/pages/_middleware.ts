a0f5a5ce14f81d14b7b35a496c2ec4890c3c984b
Jeff Everhart <Jeffeverhart383@gmail.com>
2022-06-08 09:27:17 -0400

    update logging

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,6 +2,9 @@ import { handleSitemapRequests } from '@faustjs/next';
 import { NextRequest, NextResponse } from 'next/server';
 
 export default async function _middleware(req: NextRequest) {
+  console.log(req)
+  const url = new URL(req.url)
+  console.log(url)
   const sitemapRequest = await handleSitemapRequests(req, {
     wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
     sitemapIndexPath: `/sitemap_index.xml`,
